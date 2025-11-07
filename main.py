8fd6f9f3e5dad7ba521d82a34dccfbe1cded8c89
Paulo Martins <pmartins@duck.com>
2025-11-06 21:54:31 -0300

    adiciona grupos para melhor controle

--- main.py
+++ main.py
@@ -8,7 +8,15 @@ def main():
     clock = pygame.time.Clock()
     dt=0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    
+
+
 
     while True:
         log_state()
@@ -16,8 +24,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 if __name__ == "__main__":
