7bc690552d4037eaab418129b0509394253bb502
Paulo Martins <pmartins@duck.com>
2025-11-06 21:21:27 -0300

    Criadas classes circleshape e player

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt=0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         log_state()
@@ -16,7 +16,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60)
 if __name__ == "__main__":
     main()
